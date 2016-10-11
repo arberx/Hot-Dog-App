e8dac849a35079f5e2abbb42814a344f118ff61a
Arber Xhindoli <axhindol@umich.edu>
2016-10-11 17:33:51 -0400

    new repo

--- RaysHotDogs/RaysHotDog/HotDogDetailActivity.cs
+++ RaysHotDogs/RaysHotDog/HotDogDetailActivity.cs
@@ -9,15 +9,15 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Service;
+using RaysHotDogs.Utility;
 
-namespace RaysHotDog
+namespace RaysHotDogs
 {
-
-    //set the activity label, and make sure it is launched when the app is openend. "main launcher = true"
-    [Activity(Label = "Hot dog Activity", MainLauncher = true)]
+    [Activity(Label = "Hotdog details", MainLauncher = true)]
     public class HotDogDetailActivity : Activity
     {
-        //members that will relate to the axml file named members. 
         private ImageView hotDogImageView;
         private TextView hotDogNameTextView;
         private TextView shortDescriptionTextView;
@@ -27,12 +27,70 @@ namespace RaysHotDog
         private Button cancelButton;
         private Button orderButton;
 
+        private HotDog selectedHotDog;
+        private HotDogDataService dataService;
 
-            protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
+
+            SetContentView(Resource.Layout.HotDogDetailView);
+
+            HotDogDataService dataService = new HotDogDataService();
+            selectedHotDog = dataService.GetHotDogById(1);
+
+            FindViews();
+
+            BindData();
+
+            HandleEvents();
+        }
+
+        private void FindViews()
+        {
+            hotDogImageView = FindViewById<ImageView>(Resource.Id.hotDogImageView);
+            hotDogNameTextView = FindViewById<TextView>(Resource.Id.hotDogNameTextView);
+            shortDescriptionTextView = FindViewById<TextView>(Resource.Id.shortDescriptionTextView);
+            descriptionTextView = FindViewById<TextView>(Resource.Id.descriptionTextView);
+            priceTextView = FindViewById<TextView>(Resource.Id.priceTextView);
+            amountEditText = FindViewById<EditText>(Resource.Id.amountEditText);
+            cancelButton = FindViewById<Button>(Resource.Id.cancelButton);
+            orderButton = FindViewById<Button>(Resource.Id.orderButton);
+        }
+
+        private void BindData()
+        {
+            hotDogNameTextView.Text = selectedHotDog.Name;
+            shortDescriptionTextView.Text = selectedHotDog.ShortDescription;
+            descriptionTextView.Text = selectedHotDog.Description;
+            priceTextView.Text = "Price: " + selectedHotDog.Price;
+
+            var imageBitmap = ImageHelper.GetImageBitmapFromUrl("http://gillcleerenpluralsight.blob.core.windows.net/files/" + selectedHotDog.ImagePath + ".jpg");
+
+            hotDogImageView.SetImageBitmap(imageBitmap);
+        }
+
+        private void HandleEvents()
+        {
+            orderButton.Click += OrderButton_Click;
+            cancelButton.Click += CancelButton_Click;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void OrderButton_Click(object sender, EventArgs e)
+        {
+            var amount = Int32.Parse(amountEditText.Text);
+
+            var dialog = new AlertDialog.Builder(this);
+            dialog.SetTitle("Confirmation");
+            dialog.SetMessage("Your hot dog has been added to your cart!");
+            dialog.Show();
         }
     }
 }
